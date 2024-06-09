c16617aad46b4f5e4918105c86c155b7c1ddbbdb
priyankavasam7 <priya.career.edu@gmail.com>
2024-06-09 23:33:14 +0530

    Added Assignment-4-Palindrome program

---
+++ Assignment-4-Palindrome/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignment_4_Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------Check Palindrome---------");
+            Console.WriteLine("Enter value");
+            string value=Console.ReadLine();
+            string reversedValue = "";
+            foreach (char c in value)
+            {
+                reversedValue = c + reversedValue;
+            }
+            if(reversedValue == value)
+            {
+                Console.WriteLine($"{value} is a Palindrome");
+            }
+            else
+            {
+                Console.WriteLine($"{value} is not a Palindrome");
+            }
+        }
+    }
+}
