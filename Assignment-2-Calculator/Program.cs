cc378b3e0166d5d6a483c6b5359deb7941f28e6b
priyankavasam7 <priya.career.edu@gmail.com>
2024-06-09 22:54:41 +0530

    Completed the calculator program

--- Assignment-2-Calculator/Program.cs
+++ Assignment-2-Calculator/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(".........Welcome to Calculator App.........");
+            bool flag = true;
+            do
+            {
+                Console.WriteLine("Select the operation you would like to perform: \n(Type the character shown in the brackets)");
+                Console.WriteLine("1. Addition ('+') \n2. Subtractin ('-')\n3. Multiplication ('*')\n4. Division ('/')");
+                char operation = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine("Enter first number:");
+                int firstNumber = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter second number");
+                int secondNumber = Convert.ToInt32(Console.ReadLine());
+                int result;
+                switch (operation)
+                {
+                    case '+': result= firstNumber + secondNumber;
+                        Console.WriteLine($"{firstNumber} + {secondNumber} = {result} ");
+                        break;
+                    case '-':
+                        result = firstNumber - secondNumber;
+                        Console.WriteLine($"{firstNumber} - {secondNumber} = {result} ");
+                        break;
+                    case '*':
+                        result = firstNumber * secondNumber;
+                        Console.WriteLine($"{firstNumber} * {secondNumber} = {result} ");
+                        break;
+                    case '/':
+                        result = firstNumber / secondNumber;
+                        Console.WriteLine($"{firstNumber} / {secondNumber} = {result} ");
+                        break;
+                }
+                Console.WriteLine("Would you like to continue? (Y/N)");
+                char isTrue = Convert.ToChar(Console.ReadLine());
+                if(isTrue == 'N')
+                {
+                    flag=false;
+                    Console.WriteLine("Exiting...");
+                }
+            } while (flag);
         }
     }
 }
