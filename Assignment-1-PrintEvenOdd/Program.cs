6dba43cf6528f051ad475d3564dcd74af46ab8ee
priya <priya@PRIYANKAVASAM>
2024-06-09 22:29:30 +0530

    Add project files.

---
+++ Assignment-1-PrintEvenOdd/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Assignment_1_PrintEvenOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Target number: ");
+            int target=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your choice: (\"E\" for even \"O\" for Odd)");
+            char choice=Convert.ToChar(Console.ReadLine());
+            PrintEvenOdd(target, choice);
+        }
+
+        static void PrintEvenOdd(int target, char choice)
+        {
+            int targetNumber=target;
+            char choiceSelected=choice;
+            if (choiceSelected == 'E')
+            {
+                Console.WriteLine($"Even Numbers until {targetNumber} are : ");
+                for(int i = 0;i< targetNumber; i+=2)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            else 
+            {
+                Console.WriteLine($"Odd Numbers until {targetNumber} are : ");
+                for (int i = 1; i < targetNumber; i += 2)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+        
+    }
+
+
+}
---
+++ Assignment-2-Calculator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_2_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
